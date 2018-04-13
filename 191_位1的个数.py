6d249da57e3cb34005cd85853a1f863785eedf85
laipp123 <laippmiles@gmail.com>
2018-04-13 20:41:15 +0800

    LeetCode解法

--- 191_位1的个数.py
+++ 191_位1的个数.py
@@ -10,6 +10,19 @@ class Solution(object):
             listn.append(i)
         return listn.count('1')
 
+class Solution1(object):#其实不用改列表
+    def hammingWeight(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        strn = bin(n)[2:]
+        count = 0
+        for i in strn:
+            if i == '1':
+                count += 1
+        return count
+
 class SolutionBest(object):#利用位移和逻辑运算的，难理解一点，可是快挺多
     def hammingWeight(self, n):
         """
