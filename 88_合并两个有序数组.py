91f01c8b706787bc7dd12922a7391ea3e7292f2a
laipp123 <laippmiles@gmail.com>
2018-05-22 21:29:16 +0800

    LeetCode解法

--- 88_合并两个有序数组.py
+++ 88_合并两个有序数组.py
@@ -13,6 +13,18 @@ nums2 = [2,5,6],       n = 3
 输出: [1,2,2,3,5,6]
 '''
 
+class SolutionSimple:
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: void Do not return anything, modify nums1 in-place instead.
+        """
+        tep = nums1[:m]
+        tep.extend(nums2[:])
+        nums1[:] = sorted(tep)
 
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
